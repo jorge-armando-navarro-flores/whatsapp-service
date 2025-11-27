f2ed99389a8e2472f6b807557299f2c668aef392
jorge-armando-navarro-flores <80140764+jorge-armando-navarro-flores@users.noreply.github.com>
2025-11-27 18:46:53 +0000

    Refactor imports and improve code formatting in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from dotenv import load_dotenv
-import os, httpx
+import os
+import httpx
 import asyncio
 
 load_dotenv(override=True)
@@ -11,9 +12,11 @@ VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 WHATSAPP_TOKEN = os.getenv("WHATSAPP_TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 
+
 @app.get("/")
 async def hello():
     return {"response": "Hello Whatsapp API"}
+
 
 @app.get("/webhook")
 async def verify_webhook(request: Request):
@@ -22,16 +25,17 @@ async def verify_webhook(request: Request):
         return int(params.get("hub.challenge"))
     return {"status": "Unauthorized"}, 403
 
+
 @app.post("/webhook")
 async def receive_message(req: Request):
     data = await req.json()
     print("Incoming:", data)
     try:
-        message = data['entry'][0]['changes'][0]['value']['messages'][0]
-        sender = message['from']
+        message = data["entry"][0]["changes"][0]["value"]["messages"][0]
+        sender = message["from"]
         sender = "52" + sender[3:]
         print("SENDER:", sender)
-        message_id = message['id']
+        message_id = message["id"]
 
         await mark_as_read(message_id)  # 👈 this line
 
@@ -42,17 +46,18 @@ async def receive_message(req: Request):
         print("Error:", e)
     return {"status": "received"}
 
+
 async def send_whatsapp_message(to: str, message: str):
     url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
     headers = {
         "Authorization": f"Bearer {WHATSAPP_TOKEN}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     json = {
         "messaging_product": "whatsapp",
         "to": to,
         "type": "text",
-        "text": {"body": message}
+        "text": {"body": message},
     }
     async with httpx.AsyncClient() as client:
         r = await client.post(url, headers=headers, json=json)
@@ -63,15 +68,13 @@ async def mark_as_read(message_id: str):
     url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
     headers = {
         "Authorization": f"Bearer {WHATSAPP_TOKEN}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     payload = {
         "messaging_product": "whatsapp",
         "status": "read",
         "message_id": message_id,
-        "typing_indicator": {
-            "type": "text"
-        },
+        "typing_indicator": {"type": "text"},
     }
     async with httpx.AsyncClient() as client:
         response = await client.post(url, headers=headers, json=payload)
